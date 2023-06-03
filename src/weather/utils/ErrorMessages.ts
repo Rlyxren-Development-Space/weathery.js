d2731eba20f95aa75d70199212026c0384438562
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2023-06-03 13:31:10 +0600

    Use namespace

--- src/weather/utils/ErrorMessages.ts
+++ src/weather/utils/ErrorMessages.ts
@@ -1,8 +1,24 @@
+/**
+ * Namespace for error messages.
+ * @namespace
+ */
 const ErrorMessages = {
+  /**
+   * Error message for type mismatch.
+   * @memberof ErrorMessages
+   * @property {string} message - The error message with placeholders for expected type ({ex}) and received type ({re}).
+   * @property {number} status - The HTTP status code for the error.
+   */
   typeError: {
-    message: `Expected type to be {ex} but recieved {re} instead.`,
+    message: `Expected type to be {ex} but received {re} instead.`,
     status: 400,
   },
+  /**
+   * Error message for invalid range.
+   * @memberof ErrorMessages
+   * @property {string} message - The error message with placeholders for the invalid range ({ra}) and the required range ({rae}).
+   * @property {number} status - The HTTP status code for the error.
+   */
   rangeError: {
     message: `Invalid range {ra}. Required: {rae}`,
     status: 400,
